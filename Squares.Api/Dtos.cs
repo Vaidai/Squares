ea36672be3d8c6acaab08ef09ffa43ab8832bf9e
Vaida <vaidaiBaranauskaitei@gmail.com>
2022-02-14 12:06:38 +0200

    removed unnecessary dto methods

--- Squares.Api/Dtos.cs
+++ Squares.Api/Dtos.cs
@@ -4,9 +4,4 @@ using System.ComponentModel.DataAnnotations;
 namespace Squares.Api.Dtos
 {
     public record SquareDto(Point Point1, Point Point2, Point Point3, Point Point4);
-
-
-    public record PointDto(Guid Id, int X, int Y);
-    public record AddPointDto([Required] int X, [Required] int Y);
-
 }
